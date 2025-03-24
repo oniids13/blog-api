1bcd4e171b10002eccaa5b43d47c73f43f6329c2
oniids <jdino.abaya@gmail.com>
2025-03-24 15:52:17 +0800

    minor changes

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,13 +3,16 @@ const postsRouter = Router();
 const { getAllPosts, postCreatePost, postCreateComment, postEditPost, getSinglePost, deleteSinglePost, putEditComment, deleteUserComment } = require('../controller/postsController');
 const passport = require('passport');
 
-
+// Posts route
 postsRouter.get('/', passport.authenticate('jwt', {session: false}), getAllPosts);
 postsRouter.get('/:postId', passport.authenticate('jwt', {session: false}), getSinglePost)
 postsRouter.post('/post', passport.authenticate('jwt', {session: false}), postCreatePost );
-postsRouter.post('/:postId/comment', passport.authenticate('jwt', {session: false}), postCreateComment);
 postsRouter.post('/:postId/edit', passport.authenticate('jwt', {session: false}), postEditPost);
 postsRouter.delete('/:postId/delete', passport.authenticate('jwt', {session: false}), deleteSinglePost);
+// Comments route
+postsRouter.post('/:postId/comment', passport.authenticate('jwt', {session: false}), postCreateComment);
 postsRouter.put('/comment/:commentId/edit', passport.authenticate('jwt', {session: false}), putEditComment);
 postsRouter.delete('/comment/:commentId/delete', passport.authenticate('jwt', {session: false}), deleteUserComment );
+
+
 module.exports = postsRouter;
